0db7adb6743089849424920d77e1d2dadcbfbf0e
Marcin Antosiewicz <marcinantosiewicz8@gmail.com>
2022-11-03 12:38:49 +0100

    add

--- js/script.js
+++ js/script.js
@@ -1,24 +1,33 @@
-console.log(`Hej :)`);
-
-let form = document.querySelector(".js-form");
-let sum = document.querySelector(".js-sum");
-let course= document.querySelector(".js-course");
-let sumPl = document.querySelector(".js-sumPl");
-let radioEuro = document.querySelector(".js-radio-1");
-let radioDolar = document.querySelector(".js-radio-2");
-let radioFrank = document.querySelector(".js-radio-3");
-let change = document.querySelector(".js-change");
-
-form.addEventListener("input", () => {
+{
     
+
+ const chooseCurrency = () => {
+    const change = document.querySelector(".js-change");
+    const radioEuro = document.querySelector(".js-radio-1");
+    const radioDolar = document.querySelector(".js-radio-2");
+
     change.innerText = radioEuro.checked ? "EUR" : radioDolar.checked ? "USD" : "CHF";
-    
-});
+ }
 
-// can be done without the number function
-form.addEventListener("submit", (Event) => {
-    Event.preventDefault();
+ const calculateCurrency = () => {
+    const sum = document.querySelector(".js-sum");
+    const course= document.querySelector(".js-course");
+    const sumPl = document.querySelector(".js-sumPl");
     let suma = 0;
     suma = Number(sum.value) * Number(course.value);
     sumPl.value = suma.toFixed(2);
-});
+ }
+
+ const init  = () => {
+    const form = document.querySelector(".js-form");
+    form.addEventListener("submit", (Event) => {
+        Event.preventDefault();
+        calculateCurrency();
+    });
+
+    form.addEventListener("input",  (chooseCurrency));
+ }
+
+init();
+       
+}
