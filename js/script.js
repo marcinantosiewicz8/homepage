8a93b28272a43402c0ae68f64600c496a30cde8c
Marcin Antosiewicz <marcinantosiewicz8@gmail.com>
2022-11-07 11:04:09 +0100

    add

--- js/script.js
+++ js/script.js
@@ -1,17 +1,24 @@
 {
-    
+   const clearChange = (change) => {
+      change.innerText = "Wybierz";
+    }
 
- const chooseCurrency = () => {
-    const change = document.querySelector(".js-change");
+ const chooseCurrency = (change) => {
     const radioEuro = document.querySelector(".js-radio-1");
     const radioDolar = document.querySelector(".js-radio-2");
+    const courseEuro = 4.68;
+    const courseDolar = 4.70;
+    const courseSwiss = 4.73;
 
     change.innerText = radioEuro.checked ? "EUR" : radioDolar.checked ? "USD" : "CHF";
+    course.value = radioEuro.checked ? courseEuro : radioDolar.checked ? courseDolar : courseSwiss;
+
  }
 
- const calculateCurrency = () => {
-    const sum = document.querySelector(".js-sum");
-    const course= document.querySelector(".js-course");
+
+
+ const calculateCurrency = (course) => {
+    const sum = document.querySelector(".js-sum"); 
     const sumPl = document.querySelector(".js-sumPl");
     let suma = 0;
     suma = Number(sum.value) * Number(course.value);
@@ -19,15 +26,24 @@
  }
 
  const init  = () => {
+    const change = document.querySelector(".js-change");
     const form = document.querySelector(".js-form");
+    const course= document.querySelector(".js-course");
     form.addEventListener("submit", (Event) => {
         Event.preventDefault();
-        calculateCurrency();
+        calculateCurrency(course);
     });
 
-    form.addEventListener("input",  (chooseCurrency));
+    form.addEventListener("input",  () => {
+      chooseCurrency(change);
+    });
+
+    form.addEventListener("reset", () =>{
+      clearChange(change);
+    })
  }
 
 init();
-       
+
+
 }
